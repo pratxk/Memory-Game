7c0c03aec88a0c1a827ee497a2ab5c68def3d8da
pratxk <pratxk007@gmail.com>
2024-10-01 17:33:21 +0530

    Memory Game

--- src/Pages/Quiz/Memory.jsx
+++ src/Pages/Quiz/Memory.jsx
@@ -4,66 +4,66 @@ import MemoryCard from './MemoryCard';
 
 const arr = [
     {
-        img: "https://www.shutterstock.com/editorial/image-editorial/N0zek0x4M4zcA32dMTI0NA==/bollywood-actresses-prity-zinta-international-indian-film-440nw-7859752c.jpg",
-        name: "prity",
-        id: 1
+      img:"https://www.shutterstock.com/editorial/image-editorial/N0zek0x4M4zcA32dMTI0NA==/bollywood-actresses-prity-zinta-international-indian-film-440nw-7859752c.jpg",
+      name:"prity",
+      id:1
     },
     {
-        img: "https://www.shutterstock.com/editorial/image-editorial/N0zek0x4M4zcA32dMTI0NA==/bollywood-actresses-prity-zinta-international-indian-film-440nw-7859752c.jpg",
-        name: "prity",
-        id: 2
+      img:"https://www.shutterstock.com/editorial/image-editorial/N0zek0x4M4zcA32dMTI0NA==/bollywood-actresses-prity-zinta-international-indian-film-440nw-7859752c.jpg",
+      name:"prity",
+      id:2
     },
     {
-        img: "https://m.media-amazon.com/images/M/MV5BOTYwNTc1ODE0NF5BMl5BanBnXkFtZTgwNjU3OTA3MjI@._V1_.jpg",
-        name: "jacquline",
-        id: 3
+      img:"https://m.media-amazon.com/images/M/MV5BOTYwNTc1ODE0NF5BMl5BanBnXkFtZTgwNjU3OTA3MjI@._V1_.jpg",
+      name:"jacquline",
+      id:3
     },
     {
-        img: "https://m.media-amazon.com/images/M/MV5BOTYwNTc1ODE0NF5BMl5BanBnXkFtZTgwNjU3OTA3MjI@._V1_.jpg",
-        name: "jacquline",
-        id: 4
+      img:"https://m.media-amazon.com/images/M/MV5BOTYwNTc1ODE0NF5BMl5BanBnXkFtZTgwNjU3OTA3MjI@._V1_.jpg",
+      name:"jacquline",
+      id:4
     },
     {
-        img: "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1nTvVc.img?w=768&h=1333&m=6&x=619&y=295&s=358&d=358",
-        name: "nora",
-        id: 5
+      img:"https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1nTvVc.img?w=768&h=1333&m=6&x=619&y=295&s=358&d=358",
+      name:"nora",
+      id:5
     },
     {
-        img: "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1nTvVc.img?w=768&h=1333&m=6&x=619&y=295&s=358&d=358",
-        name: "nora",
-        id: 6
+      img:"https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1nTvVc.img?w=768&h=1333&m=6&x=619&y=295&s=358&d=358",
+      name:"nora",
+      id:6
     },
     {
-        img: "https://cdn.britannica.com/75/124975-050-32A8692C/Aishwarya-Bachchan-Rai.jpg",
-        name: "Aishwariya",
-        id: 7
+      img:"https://cdn.britannica.com/75/124975-050-32A8692C/Aishwarya-Bachchan-Rai.jpg",
+      name:"Aishwariya",
+      id:7
     },
     {
-        img: "https://cdn.britannica.com/75/124975-050-32A8692C/Aishwarya-Bachchan-Rai.jpg",
-        name: "Aishwariya",
-        id: 8
+      img:"https://cdn.britannica.com/75/124975-050-32A8692C/Aishwarya-Bachchan-Rai.jpg",
+      name:"Aishwariya",
+      id:8
     },
     {
-        img: "https://www.thestatesman.com/wp-content/uploads/2024/03/Alia-Bhatt.jpg",
-        name: "Alia",
-        id: 9
+      img:"https://www.thestatesman.com/wp-content/uploads/2024/03/Alia-Bhatt.jpg",
+      name:"Alia",
+      id:9
     },
     {
-        img: "https://www.thestatesman.com/wp-content/uploads/2024/03/Alia-Bhatt.jpg",
-        name: "Alia",
-        id: 10
+      img:"https://www.thestatesman.com/wp-content/uploads/2024/03/Alia-Bhatt.jpg",
+      name:"Alia",
+      id:10
     },
     {
-        img: "https://jang.com.pk/assets/uploads/updates/2024-07-17/16224_6914659_Deepika-Padukone_updates.jpg",
-        name: "Deepika",
-        id: 11
+      img:"https://jang.com.pk/assets/uploads/updates/2024-07-17/16224_6914659_Deepika-Padukone_updates.jpg",
+      name:"Deepika",
+      id:11
     },
     {
-        img: "https://jang.com.pk/assets/uploads/updates/2024-07-17/16224_6914659_Deepika-Padukone_updates.jpg",
-        name: "Deepika",
-        id: 12
+      img:"https://jang.com.pk/assets/uploads/updates/2024-07-17/16224_6914659_Deepika-Padukone_updates.jpg",
+      name:"Deepika",
+      id:12
     },
-];
+  ]
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -78,7 +78,7 @@ const Memory = () => {
     const [score, setScore] = useState(0);
     const [matchedPairs, setMatchedPairs] = useState(new Set());
     const [cards, setCards] = useState([]);
-    const [moves, setMoves] = useState(0); // New state for tracking moves
+    const [moves, setMoves] = useState(0);
 
     useEffect(() => {
         setCards(shuffleArray([...arr])); // Shuffle only on mount
@@ -88,9 +88,8 @@ const Memory = () => {
         if (flippedIndices.length < 2 && !flippedIndices.includes(index) && !matchedPairs.has(name)) {
             setFlippedIndices([...flippedIndices, index]);
 
-            // Increment moves only after the second card is flipped
             if (flippedIndices.length === 1) {
-                setMoves(moves + 1); // Increment moves
+                setMoves(moves + 1);
 
                 const firstIndex = flippedIndices[0];
                 const firstName = cards[firstIndex].name;
@@ -98,36 +97,32 @@ const Memory = () => {
                 if (firstName === name) {
                     setScore(score + 1);
                     setMatchedPairs(new Set(matchedPairs).add(name));
-                    setFlippedIndices([]); // Clear flipped indices
+                    setFlippedIndices([]);
                 } else {
                     setTimeout(() => {
                         setFlippedIndices([]);
-                    }, 1000); // Reset flipped cards after 1 second
+                    }, 1000);
                 }
             }
         }
     };
 
     return (
-        <>
-            <Box bg='teal' p='20'>
-                <Text fontSize='xl' color='white' mb='4'>Score: {score}</Text>
-                <Text fontSize='xl' color='white' mb='4'>Moves: {moves}</Text> {/* Display moves */}
-                <Grid templateColumns='repeat(4, 1fr)' gap='20px'>
-                    {
-                        cards.map((ele, index) => (
-                            <GridItem key={ele.id}>
-                                <MemoryCard
-                                    ele={ele}
-                                    isFlipped={flippedIndices.includes(index) || matchedPairs.has(ele.name)}
-                                    onClick={() => handleCardClick(index, ele.name)}
-                                />
-                            </GridItem>
-                        ))
-                    }
-                </Grid>
-            </Box>
-        </>
+        <Box bg='teal' p={6} borderRadius="md" boxShadow="lg">
+            <Text fontSize='xl' color='white' mb={4}>Score: {score}</Text>
+            <Text fontSize='xl' color='white' mb={4}>Moves: {moves}</Text>
+            <Grid templateColumns='repeat(auto-fit, minmax(220px, 1fr))' gap={4}>
+                {cards.map((ele, index) => (
+                    <GridItem key={ele.id}>
+                        <MemoryCard
+                            ele={ele}
+                            isFlipped={flippedIndices.includes(index) || matchedPairs.has(ele.name)}
+                            onClick={() => handleCardClick(index, ele.name)}
+                        />
+                    </GridItem>
+                ))}
+            </Grid>
+        </Box>
     );
 };
 
